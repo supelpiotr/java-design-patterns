841bdb65d6854232880e18cdabb17501c0d7e1f7
supelpiotr <48559432+supelpiotr@users.noreply.github.com>
2020-04-13 17:06:09 +0200

    -converting Singleton from eagerly loaded to lazy loaded

--- main/java/singleton/Singleton.java
+++ main/java/singleton/Singleton.java
@@ -2,12 +2,16 @@ package singleton;
 
 public class Singleton {
 
-    private static Singleton instance = new Singleton();
+    private static Singleton instance = null;
 
     private Singleton() {
     }
 
     public static Singleton getInstance(){
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
         return instance;
     }
 
